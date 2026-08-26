46cf26aaad0adc0dbe7fad0522f0bd97db59f055
Tracks <tracks@local>
2026-08-26 11:01:48 +0900

    GREEN: T-010
    Tracks-Task: T-010
    Tracks-Attempt: 6
    Tracks-R: d0369f72e3d7d66f86c368ba28dcd8f464784b23
    Tracks-Issue: 96
    Tracks-AC: AC-FR0262-01,AC-FR0262-02,AC-FR0262-03,AC-NFR0140-02,FR-0262,NFR-0140

--- tracks/executor/__init__.py
+++ tracks/executor/__init__.py
@@ -16,4 +16,5 @@ from .mutation_manifest import build_manifest, validate_manifest  # noqa: F401
 
 FakeAgent = FakeBackend  # v0.1 compatibility alias
 
-__all__ = ["Executor", "FakeAgent", "FakeBackend", "git"]
+__all__ = ["Executor", "FakeAgent", "FakeBackend", "build_manifest",
+           "git", "mutation_manifest", "validate_manifest"]
